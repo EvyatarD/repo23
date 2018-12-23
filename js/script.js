c905e07abde8e9aa7cc66e98f097434b0adb4ace
evyatar <front15@sqlabs.co.il>
2018-12-23 19:29:18 +0200

    create js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-console.log(“Hello World!”);
+console.log("Hello World!");
+var populationArray = [10000,20000,30000,40000,50000];
+var maxPopulation = 50000;
+var isOverPopulated = true;
+
+for (var i = 0; i < populationArray.length; i++) {
+    if (populationArray[i]>maxPopulation)
+        console.log("larger");
+    else
+        if (populationArray[i]<maxPopulation)
+             console.log("smaller");
+        else
+        console.log("equals");
+}
+
+
+var three_city= populationArray[0]+populationArray[1]+populationArray[2];
+if (((three_city<100000) && (maxPopulation<100000)) || (isOverPopulated==false))
+    console.log("Small cities");
+else
+    console.log("Large cities!!!");
